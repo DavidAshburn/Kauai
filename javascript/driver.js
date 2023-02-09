4291f659a35e8784d896916c610591d006b2b97a
DavidAshburn <oneinchwebbing@gmail.com>
2023-02-08 17:38:30 -0800

    fragment and vDom.add() bugfixes

--- javascript/driver.js
+++ javascript/driver.js
@@ -2,19 +2,46 @@ import * as Dome from './dome.js';
 
 const vDom = Dome.init();
 
-//add(type,parent,style="",text="")
-
 vDom.add('p',0,'title flex-col1','Title Text');
 
-let maindiv = vDom.add('div',0,'flex-col1');
-vDom.add('p',maindiv,'subtitle','subtitle');
-vDom.add('p',maindiv,'subtitle','leave this alone');
+let maindiv_id = vDom.add('div',0,'flex-col1');
+let maindiv = vDom.root.dig(maindiv_id);
+maindiv.addChild(Dome.dom_el('p','subtitle','subtitle1'))
+maindiv.addChild(Dome.dom_el('p','subtitle','subtitle2'))
 
+let frag = Dome.fragment('div','flex-col1');
 
-let frag = Dome.fragment('div','flex-wide');
+let ftitle = Dome.dom_el('p','title',"fragment title");
+frag.add(frag.root,ftitle);
 
-let ftitle = frag.add(frag.root,Dome.dom_el('p','subtitle',"fragment subtitle"));
+let fsubtitle = Dome.dom_el('p','subtitle',"fragment subtitle");
+frag.add(frag.root,fsubtitle);
 
-vDom.compose(frag,maindiv);
+let subdiv = Dome.dom_el('div','flex-row');
+frag.add(frag.root,subdiv);
+
+subdiv.addChild(Dome.dom_el('p','list-item','inside fragdiv 1'));
+subdiv.addChild(Dome.dom_el('p','list-item','inside fragdiv 2'));
+
+vDom.compose(frag,maindiv_id);
 
 vDom.draw();
+
+/*
+{root
+	{title
+	}
+	{maindiv
+		subtitle1
+		subtitle2
+		{frag
+			fragment title
+			fragment subtitle
+			{subdiv
+				inside subdiv1
+				inside subdiv2
+			}
+		}
+	}
+}
+*/
